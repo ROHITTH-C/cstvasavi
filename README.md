1af617a47c8952a0b9acfdaa4c28ab912adb667f
sasikiransrinivasdulla <sasikiranworks@gmail.com>
2025-02-01 20:25:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cstvasavi
+# cstvasavi
---
+++ script.js
@@ -0,0 +1,29 @@
+function redirect(resource) {
+    const syllabusPaths = {
+        "uhv-syllabus": "pages/uhv-syllabus.html",
+        "ps-syllabus": "pages/ps-syllabus.html",
+        "os-syllabus": "pages/os-syllabus.html",
+        "dbms-syllabus": "pages/dbms-syllabus.html",
+        "se-syllabus": "pages/se-syllabus.html",
+        "es-syllabus": "pages/es-syllabus.html",
+        "fsd-syllabus": "pages/fsd-syllabus.html"
+    };
+
+    const resourcePaths = {
+        "uhv-resources": "pages/uhv-resources.html",
+        "ps-resources": "pages/ps-resources.html",
+        "os-resources": "pages/os-resources.html",
+        "dbms-resources": "pages/dbms-resources.html",
+        "se-resources": "pages/se-resources.html",
+        "es-resources": "pages/es-resources.html",
+        "fsd-resources": "pages/fsd-resources.html"
+    };
+
+    if (syllabusPaths[resource]) {
+        window.location.href = syllabusPaths[resource];
+    } else if (resourcePaths[resource]) {
+        window.location.href = resourcePaths[resource];
+    } else {
+        alert("Resource not found!");
+    }
+}
