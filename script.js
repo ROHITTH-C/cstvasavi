bb66bba94385b3972279f953d358f11d22188667
ROHITTH.C <chennupallir.ug23.cs@nitp.ac.in>
2025-02-06 22:35:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,12 +48,12 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
-    // deferredPrompt.userChoice.then((choiceResult) => {
-    //   if (choiceResult.outcome === 'accepted') {
-    //     console.log('User accepted the installation');
-    //   } else {
-    //     console.log('User dismissed the installation');
-    //   }
+     deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+       console.log('User accepted the installation');
+      } else {
+      console.log('User dismissed the installation');
+       }
       deferredPrompt = null; // Reset the prompt
     });
   });
