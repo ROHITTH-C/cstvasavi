d2c8d3917c737cccdf1ba94d2526a2e005dcd6e8
ROHITTH.C <chennupallir.ug23.cs@nitp.ac.in>
2025-02-06 20:36:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ self.addEventListener('install', (event) => {
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll([
                 '/',
-                '/index.html',
                 '/styles-v2.css',
                 '/script.js'
             ]);
